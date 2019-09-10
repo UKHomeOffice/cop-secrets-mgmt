303b0e24b37233e65a1403f47e0102c94ab2c0f0
anil.donthireddy <Anil.Donthireddy@digital.homeoffice.gov.uk>
2019-09-10 17:12:57 +0100

    Added global vars for doc_cop

--- common.py
+++ common.py
@@ -2,7 +2,7 @@
 
 import yaml
 
-global_exclusion_list = ['docker_password', 'docker_username', 'drone_private_token', 'drone_public_token', 'nginx_image', 'nginx_tag', 'quay_username', 'quay_password', 'slack_webhook']
+global_exclusion_list = ['docker_password', 'docker_username', 'doc_cop_aws_access_key_id', 'doc_cop_aws_secret_access_key','drone_private_token', 'drone_public_token', 'nginx_image', 'nginx_tag', 'quay_username', 'quay_password', 'slack_webhook']
 
 env_exclusion_list = ['analytics_site_id', 'analytics_url', 'api_cop_port', 'api_cop_protocol', 'api_cop_url', 'api_ref_port', 'api_ref_protocol', 'api_ref_url', 'drone_aws_access_key_id', 'drone_aws_secret_access_key', 'keycloak_protocol', 'keycloak_realm', 'keycloak_url', 'kube_server', 'kube_token', 'redis_port', 'redis_ssl', 'redis_token', 'redis_url', 'report_url', 'whitelist', 'www_port', 'www_protocol', 'www_url']
 
