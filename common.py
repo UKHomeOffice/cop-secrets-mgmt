47064a8f5d40dfcf14f30a060be642c0862c6c66
anil.donthireddy <Anil.Donthireddy@digital.homeoffice.gov.uk>
2019-09-11 13:47:46 +0100

    updated exclusion list

--- common.py
+++ common.py
@@ -2,7 +2,7 @@
 
 import yaml
 
-global_exclusion_list = ['docker_password', 'docker_username', 'doc_cop_aws_access_key_id', 'doc_cop_aws_secret_access_key','drone_private_token', 'drone_public_token', 'nginx_image', 'nginx_tag', 'quay_username', 'quay_password', 'report_aws_access_key_id', 'report_aws_secret_access_key', 'slack_webhook']
+global_exclusion_list = ['camunda_aws_access_key_id', 'camunda_aws_secret_access_key', 'cop_ops_sql_clients_aws_access_key_id', 'cop_ops_sql_clients_aws_secret_access_key', 'docker_password', 'docker_username', 'doc_cop_aws_access_key_id', 'doc_cop_aws_secret_access_key','drone_private_token', 'drone_public_token', 'executor_aws_access_key_id', 'executor_aws_secret_access_key', 'nginx_image', 'nginx_tag', 'quay_username', 'quay_password', 'report_aws_access_key_id', 'report_aws_secret_access_key', 'slack_webhook']
 
 env_exclusion_list = ['analytics_site_id', 'analytics_url', 'api_cop_port', 'api_cop_protocol', 'api_cop_url', 'api_ref_port', 'api_ref_protocol', 'api_ref_url', 'drone_aws_access_key_id', 'drone_aws_secret_access_key', 'keycloak_protocol', 'keycloak_realm', 'keycloak_url', 'kube_server', 'kube_token', 'redis_port', 'redis_ssl', 'redis_token', 'redis_url', 'report_url', 'whitelist', 'www_port', 'www_protocol', 'www_url']
 
