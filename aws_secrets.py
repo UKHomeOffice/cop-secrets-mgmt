df04d1d788694b8aea1b5f43a87542fa37a85b7a
Belinda Cowey <belinda.cowey@digital.homeoffice.gov.uk>
2019-09-13 13:14:33 +0100

    CB-515 Added dry run options and updated documentation

--- aws_secrets.py
+++ aws_secrets.py
@@ -32,7 +32,7 @@ def flatten_seed(data):
   return seed_list
 
 
-def processEnvSecrets(client, src_file):
+def processEnvSecrets(client, src_file, dry_run):
     with open(src_file, 'r') as seed_stream:
         seed_data = yaml.safe_load(seed_stream)
         seed_list = flatten_seed(seed_data)
@@ -80,26 +80,48 @@ def processEnvSecrets(client, src_file):
         
         try:
             secret_value = processAWSSecret(client, secret_key_name, 'list')
-            updateDroneSecret(drone_secrets_url, drone_user_token, secret_key_name, secret_value)
+            if dry_run == 'N':
+                updateDroneSecret(drone_secrets_url, drone_user_token, secret_key_name, secret_value)
             success_list.append(secret_key_name)
         except ClientError as e:
             error_list.append(secret_key_name + ' ' + e.response['Error']['Message'])
 
-    print ('\n\n')
+    print ('\n')
+
+    if dry_run != 'N':
+        print('\n**** No changes have been made, dry-run only ****\n')
+
     summaryStatus(success_list, error_list)
     if len(error_list) > 0:
         exit(1)
-    else:
-        exit(0)
+
+    exit(0)
 
 
 if __name__ == "__main__":
     secrets_list_file = os.environ.get('DRONE_WORKSPACE') + '/env.yaml'
 
+    parser = argparse.ArgumentParser(description='Secrets management')
+
+    parser.add_argument('-l', '--auth', dest='authenticate', default='N', choices=['Y', 'N'], help='Authentication required Y/N, default N')
+    parser.add_argument('-m', '--mfa', dest='mfa', help='MFA device id')
+    parser.add_argument('-p', '--primaryaccount', dest='primaryaccount', help='HO primary account')
+    parser.add_argument('-a', '--account', dest='account', help='AWS Account number')
+    parser.add_argument('-n', '--rolename', dest='rolename', help='AWS role name')
+    parser.add_argument('-d', '--dry-run', dest='dryrun', default='N', help='Only show secrets that would be updated in Drone, default N')
+
+    args = parser.parse_args()
+
     # Validate yaml file
     if not validateFile(secrets_list_file):
         exit(1)
 
-    client = getAWSSecretsManagerCreds()
+    # Get credentials
+    if args.authenticate == "Y":
+        role_arn = 'arn:aws:iam::' + args.account + ':' + args.rolename
+        mfa_arn  = 'arn:aws:iam::' + args.primaryaccount + ':mfa/' + args.mfa
+        client = getAssumeRoleCreds(role_arn, mfa_arn)
+    else:
+        client = getAWSSecretsManagerCreds('eu-west-2')
 
-    processEnvSecrets(client, secrets_list_file)
+    processEnvSecrets(client, secrets_list_file, args.dryrun)
